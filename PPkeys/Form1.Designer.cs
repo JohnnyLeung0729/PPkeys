707ef1761712c2185aa6a262ee529ac1e5bc9f41
Johnny Leung <johnnyzccx@gmial.com>
2019-06-19 22:18:53 +0800

    添加项目文件。

---
+++ PPkeys/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace PPkeys
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_test = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(568, 360);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(147, 38);
+            this.btn_test.TabIndex = 0;
+            this.btn_test.Text = "测试生成加解密";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 410);
+            this.Controls.Add(this.btn_test);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_test;
+    }
+}
+
---
+++ PPkeys/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPkeys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_test_Click(object sender, EventArgs e)
+        {
+            //RSACSPSample.RsaTest();
+
+            SM4Utils sm4u = new SM4Utils();
+            sm4u.secretKey = "JeF8U9wHFOMfs2Y8";
+
+            string sm4str = sm4u.Encrypt_ECB("你好，我测试一下鸿蒙系统，这个厕所还是很好用的，马桶也不错 goodmorning sir hello world,is very gode coder 1234567890!@#$%^&*)");
+            MessageBox.Show(sm4str);
+            string strsm4 = sm4u.Decrypt_ECB(sm4str);
+            MessageBox.Show(strsm4);
+
+            //sm4u.iv = "UISwD9fW6cFh9SNS";
+            //string sm4str1 = sm4u.Encrypt_CBC("你好，我测试一下鸿蒙系统，这个厕所还是很好用的，马桶也不错 goodmorning sir hello world,is very gode coder 1234567890!@#$%^&*)");
+            //MessageBox.Show(sm4str1);
+            //string strsm41 = sm4u.Decrypt_CBC(sm4str1);
+            //MessageBox.Show(strsm41);
+        }
+    }
+}
